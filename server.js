073f5c5e01b7de29fdaf3721e78fd3bb42090b1a
ms4905 <ms4905@gmail.com>
2017-08-23 05:13:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,67 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article one | Mehul Sadh',
+    heading: 'Article one',
+    date: '22 Aug 2017',
+    content: `<p>
+                   This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
+              </p>
+               <p>
+                   This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
+              </p>
+              <p>
+                   This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
+              </p> 
+              <p>
+                   This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
+              </p>`
+    
+}
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `<html>
+                <head> 
+                <title>
+                    ${title}
+                </title>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
+                </head>
+                <body>
+                <div class="container">
+                    <div>
+                      
+                          <a href="/">home</a>
+                      </div>
+                      <hr/>
+                      <h3>
+                        ${heading}
+                      </h3>
+                      <div>
+                        ${date}
+                      </div>
+                      <div>
+                        ${content}
+                      </div>
+                  </div>
+                </body>
+            </html>`
+            ;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res) {
-     res.sendFile(path.join(__dirname, 'ui','article-one.html'));
+     res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res) {
