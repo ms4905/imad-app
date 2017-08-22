9d0c638226c67ccadbf02eb24601573d2281b24d
ms4905 <ms4905@gmail.com>
2017-08-23 05:27:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,24 +5,40 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title: 'Article one | Mehul Sadh',
-    heading: 'Article one',
-    date: '22 Aug 2017',
-    content: `<p>
-                   This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
-              </p>
-               <p>
-                   This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
-              </p>
-              <p>
-                   This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
-              </p> 
-              <p>
-                   This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
-              </p>`
-    
-}
+var articles = {
+    'article-one':{
+         title: 'Article one | Mehul Sadh',
+        heading: 'Article one',
+        date: '22 Aug 2017',
+        content: `<p>
+                       This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
+                  </p>
+                   <p>
+                       This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
+                  </p>
+                  <p>
+                       This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
+                  </p> 
+                  <p>
+                       This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
+                  </p>`
+    },
+    'article-two':{
+         title: 'Article Two | Mehul Sadh',
+        heading: 'Article two',
+        date: '25 Aug 2017',
+        content: `<p>
+                       This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. 
+                  </p>`
+    },
+    'article-three':{ title: 'Article one | Mehul Sadh',
+        heading: 'Article one',
+        date: '22 Aug 2017',
+        content: `<p>
+                       This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content. This is the content.
+                  </p>`
+    }
+};
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
@@ -64,16 +80,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res) {
-     res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two',function(req,res) {
-     res.sendFile(path.join(__dirname, 'ui','article-two.html'));
-});
-
-app.get('/article-three',function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui','article-three.html'));
+app.get('/:articleName',function(req,res) {
+     var articleName=req.params.articleName;
+     res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
