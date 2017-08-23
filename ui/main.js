8f7e20539697acc9aa4f4b3cac96fe09e3a9e9c8
ms4905 <ms4905@gmail.com>
2017-08-24 02:13:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,26 @@
 //Counter code
 var button = document.getElementById('counter');
-var counter=0;
+
 button.onclick = function() {
     
-    // make a request to counter endpoint
+    // Create a request
+    var request = new XMLHttpRequest();
     
     //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status===200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+            
+        }
+        //No action needed
+    };
     
-    // render the variable in the correct span
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
+    // Make the request
+   request.open('GET','http://ms4905.imad.hasura-app.io/counter', true);
+   request.send(null);
 };
