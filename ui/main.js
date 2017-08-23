d31e7849ff1e26d64ca400fb0a5ec889ab186448
ms4905 <ms4905@gmail.com>
2017-08-23 20:44:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,11 @@ element.innerHTML='New Value';
 
 //Move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight() {
+    marginLeft=marginLeft + 10;
+    img.style.marginLeft=marginLeft + 'px';
+}
 madi.onclick= function() {
-    var interval=setInterval(moveLeft, 100);
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight, 100);
 };
