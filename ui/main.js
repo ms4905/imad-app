5038ed96b1ce17f4f31a7b4dc0917cca99fe5e85
ms4905 <ms4905@gmail.com>
2017-08-24 03:08:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,7 @@ button.onclick = function() {
 };
 
 //Submit name
-var nameInput=document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
@@ -56,6 +55,8 @@ submit.onclick = function() {
     };
     
     // Make the request
+    var nameInput=document.getElementById('name');
+    var name = nameInput.value;
    request.open('GET','http://ms4905.imad.hasura-app.io/submit-name?name=' + name, true);
    request.send(null);
   //Make a request to the server and send the name
